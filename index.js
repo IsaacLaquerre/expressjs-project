204a3e7e1c3566ce5760695d4d2c46441e07ca50
Isaac Laquerre <isaac.l@videotron.ca>
2021-05-09 14:52:59 -0400

    testing process environment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const hash = require('password-hash');
 //const utils = require("./utils.js") || "";
 
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: "https://expressjs-project.herokuapp.com/",
     user: process.env.dbUser || config.dbUser,
     password: process.env.dbPass || config.dbPass,
     database: process.env.dbName || config.dbName,
