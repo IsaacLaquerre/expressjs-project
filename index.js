243fa9747dffb822600674492d2a0477d0442147
Isaac Laquerre <isaac.l@videotron.ca>
2021-05-09 17:31:48 -0400

    trying stuff

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
     const body = req.body;
-    console.log(body)
+    console.log(req)
     utils.existsInTable(connection, "sessions", "email", body.email, function(exists) {
         if (!exists) {
             return res.send({
